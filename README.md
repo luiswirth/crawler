bb9066302c98554cd70729ecbf1aa954e9726b79
Luis Wirth <lwirth2000@gmail.com>
2020-04-04 17:31:47 +0200

    shared-state concurrency; accumulate findings of all tasks; request can timeout

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Crawler
 
 ## TODO
-- [x] try crawling for images
-- [ ] collect all findings from all tasks using _shared-state concurrency_
+- [x] try crawling for image_urls
+- [x] collect all findings from all tasks using _shared-state concurrency_
 - [ ] refactor into _message-passing concurrency_
+- [ ] download found images
 - [ ] running concurrent tasks on multiple threads
 - [ ] 
 - [ ] 
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,22 @@ use html5ever::tokenizer::{
     BufferQueue, Tag, TagKind, TagToken, Token, TokenSink, TokenSinkResult, Tokenizer,
     TokenizerOpts,
 };
-use std::borrow::Borrow;
 use url::{ParseError, Url};
 
 use async_std::task;
 use surf;
 
+use std::borrow::Borrow;
+use std::{
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
 type Result = std::result::Result<(), Box<dyn std::error::Error + Send + Sync + 'static>>;
 type BoxFuture = std::pin::Pin<Box<dyn std::future::Future<Output = Result> + Send>>;
+
+// goal: use _shared-state concurrency_ (multiple ownership) to accumulate the findings
+// from all tasks into one collection.
 
 #[derive(Default, Debug)]
 struct UnparsedFindings {
@@ -41,9 +49,8 @@ impl TokenSink for &mut UnparsedFindings {
                     for attribute in tag.attrs.iter() {
                         if attribute.name.local.as_ref() == "href" {
                             let url_str: &[u8] = attribute.value.borrow();
-                            self.page_links.push(
-                                String::from_utf8_lossy(url_str).into_owned(),
-                            );
+                            self.page_links
+                                .push(String::from_utf8_lossy(url_str).into_owned());
                         }
                     }
                 }
@@ -51,9 +58,8 @@ impl TokenSink for &mut UnparsedFindings {
                     for attribute in tag.attrs.iter() {
                         if attribute.name.local.as_ref() == "src" {
                             let url_str: &[u8] = attribute.value.borrow();
-                            self.image_links.push(
-                                String::from_utf8_lossy(url_str).into_owned(),
-                            );
+                            self.image_links
+                                .push(String::from_utf8_lossy(url_str).into_owned());
                         }
                     }
                 }
@@ -66,20 +72,22 @@ impl TokenSink for &mut UnparsedFindings {
 }
 
 fn parse_links(links: Vec<String>, page_url: &Url) -> Vec<Url> {
-    links.into_iter().map(|l|
-	match Url::parse(&l) {
-	    Err(ParseError::RelativeUrlWithoutBase) => page_url.join(&l).unwrap(),
-	    Err(_) => panic!("Malformed link found: {}", l),
-	    Ok(url) => url,
-	}).collect()
+    links
+        .into_iter()
+        .map(|l| match Url::parse(&l) {
+            Err(ParseError::RelativeUrlWithoutBase) => page_url.join(&l).unwrap(),
+            Err(_) => panic!("Malformed link found: {}", l),
+            Ok(url) => url,
+        })
+        .collect()
 }
 
 impl UnparsedFindings {
     fn parse(self, page_url: &Url) -> Findings {
-	Findings {
-	    page_links: parse_links(self.page_links, page_url),
-	    image_links: parse_links(self.image_links, page_url),
-	}
+        Findings {
+            page_links: parse_links(self.page_links, page_url),
+            image_links: parse_links(self.image_links, page_url),
+        }
     }
 }
 
@@ -98,42 +106,70 @@ fn process_page(page_url: &Url, page_body: String) -> Findings {
     findings
 }
 
-async fn crawl_loop(pages: Vec<Url>, current: u8, max: u8) -> Result {
-    println!("Current recursion depth: {}, max depth: {}", current, max);
+async fn crawl_loop(
+    pages: Vec<Url>,
+    current: u8,
+    max: u8,
+    findings: Arc<Mutex<Findings>>,
+) -> Result {
+    //println!("Current recursion depth: {}, max depth: {}", current, max);
 
     if current >= max {
-        println!("Reached max depth");
+        //println!("Reached max depth");
         return Ok(());
     }
 
     let mut tasks = Vec::new();
     for url in pages {
+        let findings = Arc::clone(&findings);
         let task = task::spawn(async move {
-            let mut page = surf::get(&url).await?;
+            let timeout_duration = Duration::from_millis(2000);
+            let timeout_future = async_std::future::timeout(timeout_duration, surf::get(&url));
+            let request = match timeout_future.await {
+                Ok(request) => request,
+                Err(_) => {
+                    eprintln!("Error: GET-request timeout with url `{}`", &url);
+                    return Ok(());
+                }
+            };
+            let mut page = request?;
             let body = page.body_string().await?;
             let new_findings = process_page(&url, body);
 
-            // add new_findings to findings!
+            {
+                let mut findings_inner = findings.lock().unwrap();
+                // @TODO: more idomatic way of extending all
+                findings_inner
+                    .page_links
+                    .extend_from_slice(&new_findings.page_links);
+                findings_inner
+                    .image_links
+                    .extend_from_slice(&new_findings.image_links);
+            }
 
             let new_page_links = new_findings.page_links;
-
-            box_crawl_loop(new_page_links, current + 1, max).await
+            box_crawl_loop(new_page_links, current + 1, max, findings).await
         });
-        println!("new task");
+        //println!("new task");
         tasks.push(task);
     }
 
     for task in tasks.into_iter() {
         task.await?;
-        println!("task done");
+        //println!("task done");
     }
 
     Ok(())
 }
 
 // wraper function for pinning
-fn box_crawl_loop(pages: Vec<Url>, current: u8, max: u8) -> BoxFuture {
-    Box::pin(crawl_loop(pages, current, max))
+fn box_crawl_loop(
+    pages: Vec<Url>,
+    current: u8,
+    max: u8,
+    findings: Arc<Mutex<Findings>>,
+) -> BoxFuture {
+    Box::pin(crawl_loop(pages, current, max, findings))
 }
 
 async fn fetch_images() -> Result {
@@ -144,21 +180,23 @@ async fn fetch_images() -> Result {
     Ok(())
 }
 
-
 // create findings here and pass by mutable reference to crawl_loop
 // in there aggregate all findings
 // have seperate array with all new found page_link to continue crawling
 
-//pub async fn crawl(pages: Vec<Url>, max_recursion: u8) -> BoxFuture {
-//box_crawl_loop(pages, max_recursion, 0)
-//}
+pub fn crawl(pages: Vec<Url>, max_recursion: u8) {
+    let findings = Arc::new(Mutex::new(Findings::default()));
+    task::block_on(async {
+        box_crawl_loop(pages, 0, max_recursion, Arc::clone(&findings))
+            .await
+            .unwrap();
+    });
+    dbg!(&findings);
+}
 
 fn main() -> Result {
-    task::block_on(async {
-        let urls = vec![Url::parse("https://www.rust-lang.org").unwrap()];
-        box_crawl_loop(urls, 0, 2).await.unwrap();
-        println!("done");
-    });
+    let urls = vec![Url::parse("https://www.rust-lang.org").unwrap()];
+    crawl(urls, 2);
     Ok(())
 }
 
