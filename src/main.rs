5e8c6fb527161612950ce1d9d54c7b997b3f3150
Luis Wirth <lwirth2000@gmail.com>
2020-04-06 23:55:22 +0200

    implement image fetching

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,20 @@ use html5ever::tokenizer::{
 };
 use url::{ParseError, Url};
 
-use async_std::task;
 use surf;
 
-use std::borrow::Borrow;
-use std::{
-    sync::{Arc, Mutex},
-    time::Duration,
+use async_std::{
+    task, sync::{Arc, Mutex},
+    fs::File,
+    io::prelude::*,
 };
 
-type Result = std::result::Result<(), Box<dyn std::error::Error + Send + Sync + 'static>>;
-type BoxFuture = std::pin::Pin<Box<dyn std::future::Future<Output = Result> + Send>>;
+use std::borrow::Borrow;
+use std::time::Duration;
+
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+type Result<T> = std::result::Result<T, Error>;
+type BoxFuture<T> = std::pin::Pin<Box<dyn std::future::Future<Output = Result<T>> + Send>>;
 
 // goal: use _shared-state concurrency_ (multiple ownership) to accumulate the findings
 // from all tasks into one collection.
@@ -111,7 +114,7 @@ async fn crawl_loop(
     current: u8,
     max: u8,
     findings: Arc<Mutex<Findings>>,
-) -> Result {
+) -> Result<()> {
     //println!("Current recursion depth: {}, max depth: {}", current, max);
 
     if current >= max {
@@ -137,7 +140,7 @@ async fn crawl_loop(
             let new_findings = process_page(&url, body);
 
             {
-                let mut findings_inner = findings.lock().unwrap();
+                let mut findings_inner = findings.lock().await;
                 // @TODO: more idomatic way of extending all
                 findings_inner
                     .page_links
@@ -154,6 +157,7 @@ async fn crawl_loop(
         tasks.push(task);
     }
 
+
     for task in tasks.into_iter() {
         task.await?;
         //println!("task done");
@@ -168,21 +172,43 @@ fn box_crawl_loop(
     current: u8,
     max: u8,
     findings: Arc<Mutex<Findings>>,
-) -> BoxFuture {
+) -> BoxFuture<()> {
     Box::pin(crawl_loop(pages, current, max, findings))
 }
 
-async fn fetch_images() -> Result {
-    let url = Url::parse("").unwrap();
-    let mut res = surf::get(&url).await?;
-    let img_bytes = res.body_bytes().await?;
-    dbg!(img_bytes);
+async fn fetch_images(urls: Vec<Url>) -> Result<()> {
+    let mut tasks = Vec::new();
+    for url in urls.into_iter() {
+	let task = task::spawn(async move {
+	    println!("fetching `{}`", url);
+	    let path_segments = url.path_segments().ok_or_else(|| "cannot be base")?;
+	    let file_path = path_segments.last().unwrap();
+	    let file_path = format!("imgs/{}", file_path);
+	    let mut file = File::create(file_path).await?;
+
+
+	    let timeout_duration = Duration::from_millis(2000);
+	    let timeout_future = async_std::future::timeout(timeout_duration, surf::get(&url));
+	    let request = match timeout_future.await {
+		Ok(request) => request,
+		Err(_) => {
+		    eprintln!("Error: GET-request timeout with url `{}`", &url);
+		    return Ok::<(), Error>(());
+		}
+	    };
+
+	    let mut res = request?;
+	    let img_bytes: Vec<u8> = res.body_bytes().await?;
+	    let _ = file.write(&img_bytes).await?;
+	    Ok(())
+	});
+	tasks.push(task);
+    }
+    for task in tasks.into_iter() {
+	task.await?;
+    }
     Ok(())
 }
-
-// create findings here and pass by mutable reference to crawl_loop
-// in there aggregate all findings
-// have seperate array with all new found page_link to continue crawling
 
 pub fn crawl(pages: Vec<Url>, max_recursion: u8) {
     let findings = Arc::new(Mutex::new(Findings::default()));
@@ -190,11 +216,13 @@ pub fn crawl(pages: Vec<Url>, max_recursion: u8) {
         box_crawl_loop(pages, 0, max_recursion, Arc::clone(&findings))
             .await
             .unwrap();
+	let findings = Arc::try_unwrap(findings).unwrap(); // remove Arc hull
+	let findings_inner = findings.into_inner(); // remove Mutex hull
+	fetch_images(findings_inner.image_links).await.unwrap();
     });
-    dbg!(&findings);
 }
 
-fn main() -> Result {
+fn main() -> Result<()> {
     let urls = vec![Url::parse("https://www.rust-lang.org").unwrap()];
     crawl(urls, 2);
     Ok(())
